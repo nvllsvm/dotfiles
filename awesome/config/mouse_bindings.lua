d3daeed9842edc7e6e82d6c54e2292c82d8d4fb3
Andrew Rabert <andrewr@aweber.com>
2017-10-10 13:24:49 -0400

    Goodbye Linux

--- awesome/config/mouse_bindings.lua
+++
@@ -1,6 +0,0 @@
-awful = require("awful")
-
-root.buttons(awful.util.table.join(
-    awful.button({ }, 4, awful.tag.viewnext),
-    awful.button({ }, 5, awful.tag.viewprev)
-))
--- awesome/scripts/cores.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-VALUE=`cat /proc/cpuinfo | grep -m1 cores`
-
-nVal=`expr ${VALUE:11}`
-
-
-echo $nVal
