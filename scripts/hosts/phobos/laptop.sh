79250fe83e22a1088077f9556945be95c9e07fd6
Andrew Rabert <draje@nullsum.net>
2017-10-19 00:52:34 -0400

    git-auto-update 2017-10-19 00:52:34

--- scripts/hosts/phobos/laptop.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-xrandr --output HDMI-1 --off --output DP-1 --off --output eDP-1 --primary --mode 3200x1800 --pos 0x0 --rotate normal
-external_monitor.sh 192
--- scripts/hosts/phobos/monitor.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-xrandr --output HDMI-1 --off --output DP-1 --primary --mode 1920x1200 --pos 0x0 --rotate normal --output eDP-1 --off
-external_monitor.sh 96
