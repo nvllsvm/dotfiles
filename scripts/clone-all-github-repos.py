14f29e7d50c0db516a926641d2fcfe1fbd5d7b0a
Andrew Rabert <draje@nullsum.net>
2018-01-22 21:12:58 -0500

    git-auto-update 2018-01-22 21:12:58

--- scripts/clone-all-github-repos.py
+++ scripts/clone-all-github-repos.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 import argparse
-import os
+import logging
+import pathlib
+import shutil
 import subprocess
 
+import consumers
 import requests
 
 
@@ -15,29 +18,36 @@ def get_repo_names(user):
     return sorted({repo['name'] for repo in response.json()})
 
 
-def register_with_myrepos(user, root, repo, use_ssh):
-    repo_path = os.path.join(root, repo)
-    if not os.path.isdir(repo_path):
-        if use_ssh:
-            repo_url = f'git@github.com:{user}/{repo}.git'
-        else:
-            repo_url = f'https://github.com/{user}/{repo}.git'
-        print(repo_url)
-        subprocess.run(['git', '-C', root, 'clone', repo_url])
+class RepoDownloader(consumers.Consumer):
+    def process(self, repo, user, output_path):
+        self.logger.info('Downloading %s', repo)
+        repo_path = pathlib.Path(output_path, repo)
+        shutil.rmtree(repo_path, ignore_errors=True)
+        repo_path.mkdir()
+        repo_url = f'https://github.com/{user}/{repo}.git'
+        subprocess.run(['git', '-C', output_path, 'clone', repo_url],
+                       stdout=subprocess.PIPE,
+                       stderr=subprocess.PIPE, check=True)
+        self.logger.info('Complete %s', repo)
 
 
 def main():
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s %(levelname)s %(processName)s %(message)s')
+
     parser = argparse.ArgumentParser(
             description='Clone all the repos belonging to a user.')
-    parser.add_argument('-s', '--ssh', action='store_true', dest='use_ssh')
     parser.add_argument('user')
     parser.add_argument('path')
     args = parser.parse_args()
 
-    os.makedirs(os.path.join(args.path, args.user))
+    path = pathlib.Path(args.path, args.user)
+    path.mkdir(parents=True, exist_ok=True)
 
-    for repo in get_repo_names(args.user):
-        register_with_myrepos(args.user, args.path, repo, args.use_ssh)
+    with consumers.Queue(RepoDownloader) as q:
+        for repo in get_repo_names(args.user):
+            q.put(repo, args.user, path)
 
 
 if __name__ == '__main__':
