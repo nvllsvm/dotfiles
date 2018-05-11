b0f9592a2f748ab34bb38e43895fe323eebde780
Andrew Rabert <ar@nullsum.net>
2018-05-11 00:26:07 -0400

    git-auto-update 2018-05-11 00:26:07

--- qmk_firmware/s60_x/mine/keymap.c
+++ qmk_firmware/s60_x/mine/keymap.c
@@ -1,14 +1,14 @@
-#include "s60_x.h"
+#include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    LEGACY_KEYMAP(
+    LAYOUT_legacy(
          GRV,    1,    2,   3,   4,   5,   6,   7,   8,    9,    0, MINS,  EQL, BSPC,  DEL, \
          TAB,    Q,    W,   E,   R,   T,   Y,   U,   I,    O,    P, LBRC, RBRC, BSLS,       \
          ESC,    A,    S,   D,   F,   G,   H,   J,   K,    L, SCLN, QUOT,   NO,  ENT,       \
         LSFT,   NO,    Z,   X,   C,   V,   B,   N,   M, COMM,  DOT, SLSH,   NO,   UP, RSFT, \
         LCTL, LGUI, LALT,                SPC,                  FN1, LEFT, DOWN, RGHT),
 
-    LEGACY_KEYMAP(
+    LAYOUT_legacy(
         CAPS,   F1,   F2,   F3,   F4,   F5,   F6,   F7,   F8,   F9,  F10,  F11,  F12, TRNS,  INS, \
         TRNS, MUTE, VOLD, VOLU, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,       \
         TRNS, MPRV, MPLY, MNXT, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,       \
