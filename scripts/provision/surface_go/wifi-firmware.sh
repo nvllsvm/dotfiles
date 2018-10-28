5b682be618b607ecbf5713da0db58edade9a7669
Andrew Rabert <ar@nullsum.net>
2018-10-28 11:35:35 -0400

    git-auto-update 2018-10-28 11:35:35

--- scripts/provision/surface_go/wifi-firmware.sh
+++ scripts/provision/surface_go/wifi-firmware.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 set -ex
 sudo rm -rf /usr/lib/firmware/ath10k/QCA6174/hw3.0/board-2.bin
-sudo cp board.bin /usr/lib/firmware/ath10k/QCA6174/hw3.0/board.bin
+sudo cp "$(dirname "$0")"/board.bin /usr/lib/firmware/ath10k/QCA6174/hw3.0/board.bin
