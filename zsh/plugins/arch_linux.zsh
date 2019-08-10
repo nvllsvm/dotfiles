f30fcbd0f3881f7d1f992094687456745654d700
Andrew Rabert <ar@nullsum.net>
2019-08-09 22:13:34 -0400

    git-auto-update 2019-08-10T02:13:34+00:00

--- zsh/plugins/arch_linux.zsh
+++ zsh/plugins/arch_linux.zsh
@@ -1,10 +1,4 @@
 if command -v pacman > /dev/null; then
     alias ssh='TERM=xterm-256color ssh'
-    path=("$DOTFILES/scripts/arch_linux" "$path[@]")
-
     full-update add 'arch-linux-update'
-
-    if command -v mpd > /dev/null; then
-        full-update add 'mpd-notification-update'
-    fi
 fi
