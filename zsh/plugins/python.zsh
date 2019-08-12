f64ac8d3f153c8232abf0d8406ffbb4c184e8903
Andrew Rabert <ar@nullsum.net>
2019-08-12 19:14:33 -0400

    git-auto-update 2019-08-12T23:14:33+00:00

--- zsh/plugins/python.zsh
+++ zsh/plugins/python.zsh
@@ -1,21 +1,28 @@
-export PYENV_ROOT=~/.local/git/pyenv
+export PYTHONUSERBASE=~/.local/pythonuser
 
-if [ -d "$PYENV_ROOT" ]; then
-    export PYTHONUSERBASE=~/.local/pythonuser
+if [ -d "$PYTHONUSERBASE" ]; then
+    export PYENV_ROOT=~/.local/git/pyenv
+    if [ -d "$PYENV_ROOT" ]; then
+        path=(
+            "${PYENV_ROOT}/bin"
+            "${PYENV_ROOT}/shims"
+            "$path[@]"
+        )
+        full-update add pyenv-update
+    else
+        unset PYENV_ROOT
+    fi
 
     path=(
         "${PYTHONUSERBASE}/bin"
-        "${PYENV_ROOT}/bin"
-        "${PYENV_ROOT}/shims"
         "$path[@]"
     )
 
     . ~/.local/share/pipns/shell.sh &> /dev/null || true
 
-    full-update add pyenv-update
     full-update add pip-update
     full-update add pipns-update
     full-update add 'venv --prune'
 else
-    unset PYENV_ROOT
+    unset PYTHONUSERBASE
 fi
