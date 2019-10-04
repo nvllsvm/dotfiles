5bb72ae15b995e3dcbd56b1f729a9d8ed1b81a91
Andrew Rabert <andrewr@aweber.com>
2019-10-04 17:15:21 -0400

    git-auto-update 2019-10-04T21:15:21+00:00

--- zsh/plugins/go.zsh
+++ zsh/plugins/go.zsh
@@ -1,10 +1,15 @@
+if [ -d ~/go ]; then
+    path=(
+        "$HOME/go/bin"
+        "$path[@]"
+    )
+fi
+
 export GOENV_ROOT=$HOME/.local/share/goenv
 
 if [ -d "$GOENV_ROOT" ]; then
     path=(
-        "$DOTFILES/scripts/go"
         "$GOENV_ROOT/bin"
-        "$HOME/go/bin"
         "$path[@]"
     )
 
