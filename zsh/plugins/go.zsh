50d9efd779ddf061e2af45200d3cb9cb751c40b3
Andrew Rabert <ar@nullsum.net>
2019-11-21 11:03:00 -0500

    git-auto-update 2019-11-21T16:03:00+00:00

--- zsh/plugins/go.zsh
+++ zsh/plugins/go.zsh
@@ -4,16 +4,3 @@ if [ -d ~/go ]; then
         "$path[@]"
     )
 fi
-
-export GOENV_ROOT=$HOME/.local/share/goenv
-
-if [ -d "$GOENV_ROOT" ]; then
-    path=(
-        "$GOENV_ROOT/bin"
-        "$path[@]"
-    )
-
-    full-update add goenv-update
-else
-    unset GOENV_ROOT
-fi
