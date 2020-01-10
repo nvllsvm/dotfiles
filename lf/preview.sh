726640d06bc8cd50ec88d498a3fe273b8bcc3845
Andrew Rabert <ar@nullsum.net>
2020-01-10 10:37:41 -0500

    git-auto-update 2020-01-10T15:37:41+00:00

--- lf/preview.sh
+++ lf/preview.sh
@@ -1,2 +1,17 @@
 #!/usr/bin/env sh
-exec bat --color=always --theme=base16 --plain -- "$1"
+case "$1" in
+    *.tar.xz|*tar.gz)
+        exec tar -tvf "$1"
+        ;;
+    *.xz)
+        exec xz -l -- "$1"
+        ;;
+    *.zst)
+        exec zstd -l -- "$1"
+        ;;
+    *.7z)
+        exec 7z l -- "$1"
+        ;;
+    *)
+        exec bat --color=always --theme=base16 --plain -- "$1"
+esac
