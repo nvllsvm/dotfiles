6cd3e40ff04fbf58321554763c20b94b1cc719a0
Andrew Rabert <ar@nullsum.net>
2020-03-31 14:38:21 -0400

    git-auto-update 2020-03-31T18:38:21+00:00

--- scripts/loggly/loggly_search.py
+++ scripts/loggly/loggly_search.py
@@ -5,7 +5,7 @@ import urllib.parse
 import urllib.request
 
 
-def _get_events(subdomain, token, query, start, end, order, limit):
+def _get_events(subdomain, token, query, start, end, order):
     url = 'https://{}.loggly.com/apiv2/events/iterate?{}'.format(
         subdomain,
         urllib.parse.urlencode({
@@ -14,18 +14,13 @@ def _get_events(subdomain, token, query, start, end, order, limit):
             'until': end,
             'order': order}))
 
-    events = []
     while url:
         req = urllib.request.Request(
             url,
             headers={'Authorization': f'bearer {token}'})
         body = json.load(urllib.request.urlopen(req))
         url = body.get('next')
-        for event in body.get('events', []):
-            events.append(event)
-            if limit and len(events) >= limit:
-                return events
-    return events
+        yield from body.get('events', [])
 
 
 def main():
@@ -45,10 +40,15 @@ def main():
 
     args = parser.parse_args()
 
-    events = _get_events(
+    event_iter = _get_events(
         args.subdomain, args.token, args.query, args.start, args.end,
-        args.order, args.limit)
+        args.order)
 
+    events = []
+    for event in event_iter:
+        events.append(event)
+        if args.limit and len(events) >= args.limit:
+            break
     print(json.dumps(events, indent=2, sort_keys=True))
 
 
