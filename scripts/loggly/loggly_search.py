9f57a58ae0e1ab5c719db5a0a87e0966d1526fbe
Andrew Rabert <ar@nullsum.net>
2020-03-31 14:30:36 -0400

    git-auto-update 2020-03-31T18:30:36+00:00

---
+++ scripts/loggly/loggly_search.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+import argparse
+import json
+import urllib.parse
+import urllib.request
+
+
+def _get_events(subdomain, token, query, start, end, order, limit):
+    url = 'https://{}.loggly.com/apiv2/events/iterate?{}'.format(
+        subdomain,
+        urllib.parse.urlencode({
+            'q': query,
+            'from': start,
+            'until': end,
+            'order': order}))
+
+    events = []
+    while url:
+        req = urllib.request.Request(
+            url,
+            headers={'Authorization': f'bearer {token}'})
+        body = json.load(urllib.request.urlopen(req))
+        url = body.get('next')
+        for event in body.get('events', []):
+            events.append(event)
+            if limit and len(events) >= limit:
+                return events
+    return events
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--token', required=True)
+    parser.add_argument('--subdomain', required=True)
+
+    parser.add_argument(
+        '--from', help='%(default)s',
+        default='-1h', dest='start')
+    parser.add_argument(
+        '--to', help='%(default)s', default='now', dest='end')
+    parser.add_argument(
+        '--order', help='%(default)s', choices=('asc', 'desc'), default='asc')
+    parser.add_argument('-n', type=int, dest='limit')
+    parser.add_argument('query')
+
+    args = parser.parse_args()
+
+    events = _get_events(
+        args.subdomain, args.token, args.query, args.start, args.end,
+        args.order, args.limit)
+
+    print(json.dumps(events, indent=2, sort_keys=True))
+
+
+if __name__ == '__main__':
+    main()
