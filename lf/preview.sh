deccf157e61e1e56b61baeb676e2e6c3cd1bffcd
Andrew Rabert <ar@nullsum.net>
2020-05-03 17:42:56 -0400

    git-auto-update 2020-05-03T21:42:56+00:00

--- lf/preview.sh
+++ lf/preview.sh
@@ -4,7 +4,5 @@ case "$1" in
         exec extract -l "$1"
         ;;
     *)
-        # 1024 is overkill, but using $(tput lines) doesn't seem to work consistently.
-        # results are cutoff.
         exec highlight --out-format=ansi -- "$1"
 esac
