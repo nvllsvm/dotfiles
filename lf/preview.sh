af028b0bfc37a68261eed01ff05da8744e4cb32c
Andrew Rabert <ar@nullsum.net>
2020-05-04 00:32:19 -0400

    git-auto-update 2020-05-04T04:32:19+00:00

--- lf/preview.sh
+++ lf/preview.sh
@@ -7,5 +7,7 @@ case "$1" in
         exec extract -l "$1"
         ;;
     *)
-        exec highlight --out-format=ansi -- "$1"
+        # set line count to avoid unnecessary page length.
+        # 1024 is overkill, but using $(tput lines) is not reliable.
+        exec bat -r :1024 --color=always --plain -- "$1"
 esac
