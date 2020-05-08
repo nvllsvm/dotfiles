de19553c199cb9e527fb3e19bc5888c2a2c2fe0c
Andrew Rabert <ar@nullsum.net>
2020-05-08 19:28:41 -0400

    git-auto-update 2020-05-08T23:28:41+00:00

--- lf/preview.sh
+++ lf/preview.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env sh
 case "$1" in
-    *.tar.zst)
-        exec tar -tvf "$1"
-        ;;
-    *.tar.xz|*tar.gz|*.xz|*.zst|*.7z|*.zip|*.iso)
+    *.tar.*|*.xz|*.zst|*.7z|*.zip|*.iso)
         exec extract -l "$1"
         ;;
     *)
