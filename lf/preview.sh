e96ccac8fa822b232ca5692ca685c8d4b78413e1
Andrew Rabert <ar@nullsum.net>
2020-06-19 10:03:40 -0400

    git-auto-update 2020-06-19T14:03:40+00:00

--- lf/preview.sh
+++ lf/preview.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env sh
 if [ -d "$1" ]; then
-    cd "$1"
+    cd "$1" || exit 1
     fd --color=always --maxdepth=1
     exit
+else
+    case "$1" in
+        *.tar.*|*.rar|*.7z|*.zip|*.iso|*.zst|*.exe|*.pk3)
+            extract -l "$1"
+            ;;
+        *)
+            # set line count to avoid unnecessary page length.
+            # 1024 is overkill, but using $(tput lines) is not reliable.
+            bat -r :1024 --color=always --plain -- "$1"
+    esac
 fi
-case "$1" in
-    *.tar.*|*.rar|*.7z|*.zip|*.iso|*.zst|*.exe|*.pk3)
-        if extract -l "$1"; then
-            exit
-        fi
-        ;;
-esac
-# set line count to avoid unnecessary page length.
-# 1024 is overkill, but using $(tput lines) is not reliable.
-exec bat -r :1024 --color=always --plain -- "$1"
