bab2ac53f02a74d843dfaf891884f69a191d679e
Andrew Rabert <ar@nullsum.net>
2020-06-19 10:02:09 -0400

    git-auto-update 2020-06-19T14:02:09+00:00

--- lf/preview.sh
+++ lf/preview.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env sh
+if [ -d "$1" ]; then
+    cd "$1"
+    fd --color=always --maxdepth=1
+    exit
+fi
 case "$1" in
     *.tar.*|*.rar|*.7z|*.zip|*.iso|*.zst|*.exe|*.pk3)
         if extract -l "$1"; then
