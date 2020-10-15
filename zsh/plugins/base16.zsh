b3a5f9a550be2d0f13e479543ca76cccc51296b1
Andrew Rabert <ar@nullsum.net>
2020-10-14 23:20:38 -0400

    git-auto-update 2020-10-15T03:20:38+00:00

--- zsh/plugins/base16.zsh
+++ zsh/plugins/base16.zsh
@@ -3,7 +3,7 @@ export BASE16_SHELL="$HOME/.local/share/base16-shell/"
 if [ -d "$BASE16_SHELL" ]; then
     export BASE16_SHELL_HOOKS="$DOTFILES/base16_shell_hooks"
 
-    if [ -z $SSH_TTY ] && [ -z $TMUX ] && [ -f ~/.base16_theme ]; then
+    if [ -z "$SSH_CONNECTION" ] && [ -z "$TMUX" ] && [ -f ~/.base16_theme ]; then
         . ~/.base16_theme
     fi
 
