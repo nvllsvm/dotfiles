576833e1f6490d7a6c0547d5f2938719b77026f4
Andrew Rabert <ar@nullsum.net>
2020-11-08 17:46:47 -0500

    git-auto-update 2020-11-08T22:46:47+00:00

--- lf/preview.sh
+++ lf/preview.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
+set -e
 if [ -d "$1" ]; then
     cd "$1" || exit 1
     fd --color=always --maxdepth=1
@@ -7,6 +8,9 @@ else
     case "$1" in
         *.tar.*|*.rar|*.7z|*.zip|*.iso|*.zst|*.exe|*.pk3|*.tar|*.tgz|*.cbz)
             extract -l "$1"
+            ;;
+        *.mkv)
+            mkvmerge -J "$1" | jq --sort-keys --color-output
             ;;
         *)
             # set line count to avoid unnecessary page length.
