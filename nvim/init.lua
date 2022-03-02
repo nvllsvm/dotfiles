3de0446dccd7ca9c8934e260095bcc55fcf6d764
Andrew Rabert <ar@nullsum.net>
2022-03-02 15:49:16 -0500

    git-auto-update 2022-03-02T20:49:16+00:00

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,6 @@
-local fn = vim.fn
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+local install_path = vim.fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  packer_bootstrap = vim.fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
 end
 
 vim.o.ruler = true
