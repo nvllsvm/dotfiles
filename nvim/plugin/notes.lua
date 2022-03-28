28bda7abf581c654af07be9cfeca370e30ac9faf
Andrew Rabert <ar@nullsum.net>
2022-03-28 11:05:52 -0400

    add journal command

--- nvim/plugin/notes.lua
+++ nvim/plugin/notes.lua
@@ -2,3 +2,6 @@ notes_dir = os.getenv("NOTES_DIR") or '~/notes'
 function Journal()
     vim.cmd('e ' .. notes_dir .. "/journal/" .. os.date("%Y-%m-%d") .. ".md")
 end
+vim.cmd([[
+command Journal lua Journal()
+]])
