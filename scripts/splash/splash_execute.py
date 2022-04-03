8f0825d3cd688027ddfc5f4584819d6d50446042
Andrew Rabert <ar@nullsum.net>
2022-04-03 11:26:42 -0400

    Asyncify

--- scripts/splash/splash_execute.py
+++ scripts/splash/splash_execute.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
 import argparse
+import asyncio
 import json
 
-import requests
+import httpx
 
 
 class Splash:
     def __init__(self, url):
         self.url = url
+        self.client = httpx.AsyncClient()
 
-    def execute(self, url, wait=0.5):
+    async def execute(self, url, wait=0.5):
         lua = """
         function main(splash, args)
           assert(splash:go(args.url))
@@ -27,7 +29,7 @@ class Splash:
             'url': url,
             'wait': wait,
         }
-        response = requests.post(f'{self.url}/execute', json=body)
+        response = await self.client.post(f'{self.url}/execute', json=body)
         return response.json()
 
 
@@ -35,15 +37,15 @@ def pretty_json(data):
     return json.dumps(data, indent=2, sort_keys=True, ensure_ascii=False)
 
 
-def main():
+async def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--splash', metavar='URL', required=True)
     parser.add_argument('url')
     args = parser.parse_args()
 
     splash = Splash(url=args.splash)
-    print(pretty_json(splash.execute(args.url)))
+    print(pretty_json(await splash.execute(args.url)))
 
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
