0f21c5199398accc649627d7d7ac9edd1c2df3b5
Andrew Rabert <ar@nullsum.net>
2022-08-25 18:40:50 -0400

    git-auto-update 2022-08-25T22:40:50+00:00

--- scripts/random_mac_address.py
+++ scripts/random_mac_address.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
 import random
-
-CHARS = ['a','b','c','d','e','f','0','1','2','3','4','5','6','7','8','9']
+import string
 
 
 def rmac():
     return ':'.join(
-        random.choice(CHARS) + random.choice(CHARS)
+        random.choice(string.hexdigits) + random.choice(string.hexdigits)
         for _ in range(6)
-    )
+    ).lower()
 
 
 print(rmac())
