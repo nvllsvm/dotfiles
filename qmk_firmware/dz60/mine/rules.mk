b70dbc779cf638acc56e90cf4c24e37fcf30f108
Andrew Rabert <ar@nullsum.net>
2022-10-20 13:12:33 -0400

    update to qmk d13f5f0d662175a2d7b4fc93941dc996bfa92ff2

--- qmk_firmware/dz60/mine/rules.mk
+++ qmk_firmware/dz60/mine/rules.mk
@@ -1,2 +1,8 @@
 BACKLIGHT_ENABLE = no
 RGBLIGHT_ENABLE = no
+
+# prevents mouse device from being exposed to host
+MOUSEKEY_ENABLE = no
+
+# prevents multiple HID devices from being exposed to host
+KEYBOARD_SHARED_EP = yes
