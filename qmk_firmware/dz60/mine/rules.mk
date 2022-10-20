adbbd77af01147c22458d475c3f91989e5749bc2
Andrew Rabert <ar@nullsum.net>
2022-10-20 19:45:30 -0400

    git-auto-update 2022-10-20T23:45:30+00:00

--- qmk_firmware/dz60/mine/rules.mk
+++ qmk_firmware/dz60/mine/rules.mk
@@ -1,8 +1,6 @@
 BACKLIGHT_ENABLE = no
 RGBLIGHT_ENABLE = no
 
+# mouse not used in layout.
 # prevents mouse device from being exposed to host
 MOUSEKEY_ENABLE = no
-
-# prevents multiple HID devices from being exposed to host
-KEYBOARD_SHARED_EP = yes
