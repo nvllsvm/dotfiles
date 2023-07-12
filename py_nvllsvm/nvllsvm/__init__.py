5bb2d2c9ac8c1626b36ee3f2f51051f89bf1946c
Andrew Rabert <ar@nullsum.net>
2023-07-12 16:09:31 -0400

    git-auto-update 2023-07-12T20:09:31+00:00

--- py_nvllsvm/nvllsvm/__init__.py
+++
@@ -1,4 +0,0 @@
-from .all_paths import all_paths
-from .asyncio_executor import AsyncExecutor
-from .chunked import chunked
-from .pretty_json import pretty_json
--- py_nvllsvm/nvllsvm/chunked.py
+++
@@ -1,7 +0,0 @@
-import itertools
-
-
-def chunked(iterable, max_size):
-    i = iter(iterable)
-    while result := list(itertools.islice(i, max_size)):
-        yield result
