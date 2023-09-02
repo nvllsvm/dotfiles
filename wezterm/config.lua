eb86feaa30f663495fadcc353ed7c569c74db213
Andrew Rabert <ar@nullsum.net>
2023-09-01 22:29:56 -0400

    git-auto-update 2023-09-02T02:29:56+00:00

---
+++ wezterm/config.lua
@@ -0,0 +1,18 @@
+local wezterm = require 'wezterm'
+
+local config = {}
+
+config.enable_scroll_bar = false
+config.enable_tab_bar = false
+config.check_for_updates = false
+config.window_close_confirmation = 'NeverPrompt'
+config.font = wezterm.font('monospace', { })
+config.font_size = 12
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
+
+return config
