9326386e16fad7eb8ff27c604228aa746cec5ab5
Andrew Rabert <ar@nullsum.net>
2024-07-12 12:42:03 -0400

    git-auto-update 2024-07-12T16:42:03+00:00

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -27,9 +27,6 @@ require("lazy").setup({
     -- import your plugins
     { import = "plugins" },
   },
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "habamax" } },
   -- automatically check for plugin updates
-  checker = { enabled = true },
+  checker = { enabled = false },
 })
