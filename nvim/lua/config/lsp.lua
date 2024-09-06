26f123701f1c7255f14a23abd417c672f063cba0
Andrew Rabert <ar@nullsum.net>
2024-09-06 15:28:38 -0400

    git-auto-update 2024-09-06T19:28:38+00:00

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -38,7 +38,7 @@ end
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
-local servers = { "rust_analyzer", "tsserver", "pyright" }
+local servers = { "rust_analyzer", "ts_ls", "pyright" }
 for _, lsp in ipairs(servers) do
     nvim_lsp[lsp].setup({
         on_attach = on_attach,
